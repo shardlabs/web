0291b451095917958120d1c976d295e427439126
Jamie Shepherd <itsjamieshepherd@gmail.com>
2025-09-12 20:58:01 -0700

    initial commit

---
+++ lib/layout.shared.tsx
@@ -0,0 +1,52 @@
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { BookIcon } from "lucide-react";
+
+/**
+ * Shared layout configurations
+ *
+ * you can customise layouts individually from:
+ * Home Layout: app/(home)/layout.tsx
+ * Docs Layout: app/docs/layout.tsx
+ */
+export function baseOptions(): BaseLayoutProps {
+  return {
+    nav: {
+      title: (
+        <>
+          <svg
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-label="Logo"
+          >
+            <circle cx={12} cy={12} r={12} fill="currentColor" />
+          </svg>
+          My App
+        </>
+      ),
+    },
+    // see https://fumadocs.dev/docs/ui/navigation/links
+    links: [
+      {
+        icon: <BookIcon />,
+        text: "Documentation",
+        url: "/docs",
+      },
+      {
+        icon: <BookIcon />,
+        text: "Blog",
+        url: "/blog",
+        // secondary items will be displayed differently on navbar
+        secondary: false,
+      },
+      {
+        icon: <BookIcon />,
+        text: "Roadmap",
+        url: "/roadmap",
+        // secondary items will be displayed differently on navbar
+        secondary: false,
+      },
+    ],
+    githubUrl: "https://github.com/shardlabs/web",
+  };
+}
---
+++ source.config.ts
@@ -0,0 +1,35 @@
+import {
+  defineCollections,
+  defineConfig,
+  defineDocs,
+  frontmatterSchema,
+  metaSchema,
+} from "fumadocs-mdx/config";
+import { z } from "zod";
+
+// You can customise Zod schemas for frontmatter and `meta.json` here
+// see https://fumadocs.dev/docs/mdx/collections#define-docs
+export const docs = defineDocs({
+  docs: {
+    schema: frontmatterSchema,
+  },
+  meta: {
+    schema: metaSchema,
+  },
+});
+
+export const blogPosts = defineCollections({
+  type: "doc",
+  dir: "content/blog",
+  // add required frontmatter properties
+  schema: frontmatterSchema.extend({
+    author: z.string(),
+    date: z.iso.date().or(z.date()),
+  }),
+});
+
+export default defineConfig({
+  mdxOptions: {
+    // MDX options
+  },
+});
