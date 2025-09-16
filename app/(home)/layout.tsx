6348233768aca99f4054984f92520bf8e122b77d
Jamie Shepherd <itsjamieshepherd@gmail.com>
2025-09-16 14:32:13 -0700

    Add bun driven workflow

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,25 +1,25 @@
-import { baseOptions as sharedBaseOptions } from "@/lib/layout.shared";
-import { HomeLayout } from "fumadocs-ui/layouts/home";
-import { BaseLayoutProps } from "fumadocs-ui/layouts/links";
+import { baseOptions as sharedBaseOptions } from '@/lib/layout.shared';
+import { HomeLayout } from 'fumadocs-ui/layouts/home';
+import { BaseLayoutProps } from 'fumadocs-ui/layouts/links';
 
-export default function Layout({ children }: LayoutProps<"/">) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+function homeBaseOptions(): BaseLayoutProps {
+    return {
+        ...sharedBaseOptions(),
+        links: [
+            {
+                text: 'Documentation',
+                url: '/docs',
+            },
+            {
+                text: 'Blog',
+                url: '/blog',
+                // secondary items will be displayed differently on navbar
+                secondary: false,
+            },
+        ],
+    };
 }
 
-export function baseOptions(): BaseLayoutProps {
-  return {
-    ...sharedBaseOptions(),
-    links: [
-      {
-        text: "Documentation",
-        url: "/docs",
-      },
-      {
-        text: "Blog",
-        url: "/blog",
-        // secondary items will be displayed differently on navbar
-        secondary: false,
-      },
-    ],
-  };
+export default function Layout({ children }: LayoutProps<'/'>) {
+    return <HomeLayout {...homeBaseOptions()}>{children}</HomeLayout>;
 }
