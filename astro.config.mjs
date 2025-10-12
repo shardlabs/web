abe65050b2c754c709cca377e844e697661774e3
Jamie Shepherd <me@jamie.sh>
2025-10-11 19:01:11 -0700

    Add favicon, use Inter font

--- astro.config.mjs
+++ astro.config.mjs
@@ -66,6 +66,7 @@ export default defineConfig({
                     footerText: '',
                 }),
             ],
+            customCss: ['./src/styles/docs.css'],
         }),
         react(),
         mdx(),
