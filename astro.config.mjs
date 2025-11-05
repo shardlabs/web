f40d543d7508257ba5ab6f7158c53013f785bb12
Jamie Shepherd <me@jamie.sh>
2025-11-05 08:38:16 -0800

    Link inter font

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,6 +28,13 @@ export default defineConfig({
                         defer: true,
                     },
                 },
+                {
+                    tag: 'link',
+                    attrs: {
+                        rel: 'stylesheet',
+                        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap',
+                    },
+                },
             ],
             sidebar: [
                 { label: 'Start Here', items: [{ slug: 'docs' }] },
